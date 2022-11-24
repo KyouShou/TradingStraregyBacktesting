13db9cefcb92460c231af4f150cfed72f5562048
KyouShou <j_123321@hotmail.com>
2022-11-24 17:02:33 +0800

    Build the project and setup gitignore

---
+++ Tests/TradingStraregyBacktesting.test/_Tests.cs
@@ -0,0 +1,16 @@
+namespace TradingStraregyBacktesting.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
